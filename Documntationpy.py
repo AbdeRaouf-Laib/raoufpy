c28e5619556863569019c34a277f1d0108c0e92e
AbdeRaouf-Laib <laibabderaouf2004@gmail.com>
2023-09-19 08:18:51 +0100

    add

--- Documntationpy.py
+++ Documntationpy.py
@@ -9,9 +9,25 @@ if 2 < 5:
 #if 2 < 5:
 #print("2 < 5")
  
+#The comments: use # to comment a one line and use """ to comment multi line 
+
 #The varibles
 x = 2
+X = 4
 y = "hello world"
+z = 'hay, can you help me'
+#To specify the data type of the variable
+x = int(3) # x will be 3
+y = str(3) # x will be '3'
+z = float(3) #x will be 3.0
+Name, familyName = "raouf","laib"
+a = b = 2
+Fruit = ["apple","banana"]
+x,y = Fruit
+print(x,y) 
+#To get the data type of the variable
+x = 3
+print(type(x))
 
 
 
