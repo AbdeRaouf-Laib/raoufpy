f55bafdae67761927f521a8299b978c38664bd65
AbdeRaouf-Laib <laibabderaouf2004@gmail.com>
2023-09-19 04:53:01 +0100

    adding

--- Documntationpy.py
+++ Documntationpy.py
@@ -1,1 +1,25 @@
+#First code in python:
 print("hello world!")
+
+#Indentation is very important in PY because it is used to denote a block of code.
+if 2 < 5:
+    print("2 < 5")
+
+#ERROR:
+#if 2 < 5:
+#print("2 < 5")
+ 
+#The varibles
+x = 2
+y = "hello world"
+
+
+
+
+#COMMENTS:
+#The number of spaces is up to use as a programmer but it has to be at least one; 
+#And we have to use the same of spaces number in the block of code! 
+if 5 < 2:
+    print("5 < 2")
+if 2 < 5:
+        print("5 < 2")
