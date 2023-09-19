e797a61a016e5edb13413cc4a2b9a09e69d0db64
AbdeRaouf-Laib <laibabderaouf2004@gmail.com>
2023-09-19 11:36:54 +0100

    add

--- Documntationpy.py
+++ Documntationpy.py
@@ -14,20 +14,73 @@ if 2 < 5:
 #The varibles
 x = 2
 X = 4
-y = "hello world"
+y = "hello world "
 z = 'hay, can you help me'
 #To specify the data type of the variable
 x = int(3) # x will be 3
 y = str(3) # x will be '3'
-z = float(3) #x will be 3.0
+d = float(3) #x will be 3.0
 Name, familyName = "raouf","laib"
 a = b = 2
 Fruit = ["apple","banana"]
 x,y = Fruit
-print(x,y) 
+
 #To get the data type of the variable
 x = 3
-print(type(x))
+type(x)
+
+#The output of varible
+print(x)
+print(x,y)
+print("int ",a + b)
+print("string ",y + z)
+
+#GLobal varible and local varible:
+m = "hello" #Global varible
+print(m)
+def var_fonction():
+    mn = 4 #Locale varible 
+    print(mn, m)
+    
+
+var_fonction()
+
+#To create a global variable inside a function:
+def My_fon():
+    global s 
+    s = 5
+My_fon()
+print(s)
+
+#Data Type:
+
+#Text Type:	    str
+#Numeric Types:	int, float, complex
+#Sequence Types:list, tuple, range
+#Mapping Type:  dict
+#Set Types:	    set, frozenset
+#Boolean Type:	bool
+#Binary Types:	bytes, bytearray, memoryview
+#None Type:	    NoneType
+
+#Example:
+x = "Hello World"	                          #str	
+x = 20	                                      #int	
+x = 20.5	                                  #float	
+x = 1j	                                      #complex	
+x = ["apple", "banana", "cherry"]	          #list	
+x = ("apple", "banana", "cherry")	          #tuple	
+x = range(6)	                              #range	
+x = {"name" : "John", "age" : 36}	          #dict	
+x = {"apple", "banana", "cherry"}	          #set	
+x = frozenset({"apple", "banana", "cherry"})  #frozenset	
+x = True                	                  #bool	
+x = b"Hello"	                              #bytes	
+x = bytearray(5)	                          #bytearray	
+x = memoryview(bytes(5))	                  #memoryview	
+x = None
+
+
 
 
 
