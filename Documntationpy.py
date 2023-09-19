e0d73237a2e4ac51537ef6e26c336d1b4a537420
AbdeRaouf-Laib <laibabderaouf2004@gmail.com>
2023-09-19 12:01:19 +0100

    add

--- Documntationpy.py
+++ Documntationpy.py
@@ -79,16 +79,58 @@ x = b"Hello"	                              #bytes
 x = bytearray(5)	                          #bytearray	
 x = memoryview(bytes(5))	                  #memoryview	
 x = None
-
-
+#random function
+import random
+#random.radrange(first num,secound num)
+print("ran",random.randrange(1, 100))
+#You can assign a multiline string to a variable by using three quotes:
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+  #or
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+#for get only one varible in python
+print("var",a[5])
+# we can loop through the characters in a string:
+for x in "banana":
+  print(x)
+#To get the length of a string, use the len() function:
+a = "Hello, World!"
+print(len(a))
+#To check if a certain phrase or character is present in a string, we can use the keyword in:
+txt = "The best things in life are free!"
+print("free" in txt)#The result is True or False
 
 
 
 
 #COMMENTS:
-#The number of spaces is up to use as a programmer but it has to be at least one; 
+#-The number of spaces is up to use as a programmer but it has to be at least one; 
 #And we have to use the same of spaces number in the block of code! 
 if 5 < 2:
     print("5 < 2")
 if 2 < 5:
         print("5 < 2")
+#-python is accepte the power of 10 and he he was write:
+power = 12e4
+ #or 
+powera = 12E4
+#-You can convert from one type to another with the int(), float(), and complex() methods and You cannot convert complex numbers into another number type.:
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
