0dbc47615005002e2bb846486c25371271c51a6b
AbdeRaouf-Laib <laibabderaouf2004@gmail.com>
2023-09-26 19:50:03 +0100

    add

--- Documntationpy.py
+++ Documntationpy.py
@@ -965,6 +965,75 @@ for x in thisdict.values():
 for x, y in thisdict.items():
   print(x, y)
 
+#You cannot copy a dictionary simply by typing dict2 = dict1, because: dict2 will only be a reference to dict1, and changes made in dict1 will automatically also be made in dict2.
+
+#Make a copy of a dictionary with the copy() method:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = thisdict.copy()
+print(mydict)
+
+#Make a copy of a dictionary with the dict() function:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = dict(thisdict)
+print(mydict)
+
+#A dictionary can contain dictionaries, this is called nested dictionaries:
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+#Or, if you want to add three dictionaries into a new dictionary:
+child1 = {
+  "name" : "Emil",
+  "year" : 2004
+}
+child2 = {
+  "name" : "Tobias",
+  "year" : 2007
+}
+child3 = {
+  "name" : "Linus",
+  "year" : 2011
+}
+myfamily = {
+  "child1" : child1,
+  "child2" : child2,
+  "child3" : child3
+}
+print(myfamily["child2"]["name"])
+
+#Dictionary Methods
+
+#Method	        Description
+#clear()	      Removes all the elements from the dictionary
+#copy()	        Returns a copy of the dictionary
+#fromkeys()	    Returns a dictionary with the specified keys and value
+#get()	        Returns the value of the specified key
+#items()	      Returns a list containing a tuple for each key value pair
+#keys()	        Returns a list containing the dictionary's keys
+#pop()	        Removes the element with the specified key
+#popitem()	    Removes the last inserted key-value pair
+#setdefault()   Returns the value of the specified key. If the key does not exist: insert the key, with the specified value
+#update()       Updates the dictionary with the specified key-value pairs
+#values()       Returns a list of all the values in the dictionary
 
 
 
