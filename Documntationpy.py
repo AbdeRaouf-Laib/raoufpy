87685a3ee95636d83d02dd388dbbe296bcb0298c
AbdeRaouf-Laib <laibabderaouf2004@gmail.com>
2023-09-26 14:41:13 +0100

    add

--- Documntationpy.py
+++ Documntationpy.py
@@ -586,7 +586,27 @@ thistuple = tuple(y)
 #The del keyword can delete the tuple completely:
 thistuple = ("apple", "banana", "cherry")
 del thistuple
-print(thistuple) #this will raise an error because the tuple no longer exists
+
+#in Python, we are also allowed to extract the values back into variables. This is called "unpacking":
+fruits = ("apple", "banana", "cherry")
+(green, yellow, red) = fruits
+print(green)
+print(yellow)
+print(red)
+#Note: The number of variables must match the number of values in the tuple, if not, you must use an asterisk to collect the remaining values as a list.
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+(green, yellow, *red) = fruits
+print(green)
+print(yellow)
+print(red)
+#Add a list of values the "tropic" variable:
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+(green, *tropic, red) = fruits
+print(green)
+print(tropic)
+print(red)
+
+#
 
 
 
