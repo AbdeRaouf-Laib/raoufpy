4cbaa6e1a66688b4fdb6639d8e22a19beca92738
AbdeRaouf-Laib <laibabderaouf2004@gmail.com>
2023-09-27 16:04:24 +0100

    add

--- Documntationpy.py
+++ Documntationpy.py
@@ -1426,6 +1426,96 @@ myiter = iter(myclass)
 for x in myiter:
   print(x)
 
+# The try block lets you test a block of code for errors.
+# The except block lets you handle the error.
+# The else block lets you execute code when there is no error.
+# The finally block lets you execute code, regardless of the result of the try- and except blocks.
+try:
+  print(x)
+except NameError:
+  print("Variable x is not defined")
+except:
+  print("Something else went wrong")
+ #or
+try:
+  print("Hello")
+except:
+  print("Something went wrong")
+else:
+  print("Nothing went wrong")
+#or
+try:
+  print(x)
+except:
+  print("Something went wrong")
+finally:
+  print("The 'try except' is finished")
+#Raise an exception
+# As a Python developer you can choose to throw an exception if a condition occurs.
+# To throw (or raise) an exception, use the raise keyword.
+# Raise an error and stop the program if x is lower than 0:
+# x = -1
+# if x < 0:
+#   raise Exception("Sorry, no numbers below zero")
+
+x = "hello"
+
+# if not type(x) is int:
+#   raise TypeError("Only integers are allowed")
+
+#user input
+username = input("Enter username:")
+print("Username is: " + username)
+
+# To make sure a string will display as expected, we can format the result with the format() method.
+# String format()
+# The format() method allows you to format selected parts of a string.
+# Sometimes there are parts of a text that you do not control, maybe they come from a database, or user input?
+# To control such values, add placeholders (curly brackets {}) in the text, and run the values through the format() method:
+# Get your own Python Server
+# Add a placeholder where you want to display the price:
+price = 49
+txt = "The price is {} dollars"
+print(txt.format(price))
+
+# You can add parameters inside the curly brackets to specify how to convert the value:
+# Format the price to be displayed as a number with two decimals:
+txt = "The price is {:.2f} dollars"
+
+# Check out all formatting types in our String format() Reference.
+# Multiple Values
+# If you want to use more values, just add more values to the format() method:
+count = 2
+print(txt.format(price, itemno, count))
+
+# And add more placeholders:
+quantity = 3
+itemno = 567
+price = 49
+myorder = "I want {} pieces of item number {} for {:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
+
+# Index Numbers
+# You can use index numbers (a number inside the curly brackets {0}) to be sure the values are placed in the correct placeholders:
+quantity = 3
+itemno = 567
+price = 49
+myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
+
+# print(myorder.format(quantity, itemno, price))
+# Also, if you want to refer to the same value more than once, use the index number:
+age = 36
+name = "John"
+txt = "His name is {1}. {1} is {0} years old."
+print(txt.format(age, name))
+
+# Named Indexes
+# You can also use named indexes by entering a name inside the curly brackets {carname}, but then you must use names when you pass the parameter values txt.format(carname = "Ford"):
+myorder = "I have a {carname}, it is a {model}."
+print(myorder.format(carname = "Ford", model = "Mustang"))
+
+
+
 
 
 
